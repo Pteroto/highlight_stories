06af7c6b1ead899cb73918502b37373b18a695be
Erik Balthazar <erik.balthazar@dextra-sw.com>
2020-08-13 20:11:22 -0300

    updating firestore url, its token and fixing video slugs

--- lib/main/urls.dart
+++ lib/main/urls.dart
@@ -1,9 +1,9 @@
 class Urls {
-  final List<String> videos = ["canal-brasil", "gnt", "gloob", "gnews", "multishow", "off", "studiouniversal", "viva"];
+  final List<String> videos = ["canal-brasil", "gnt", "gloob", "globonews", "multishow", "canal-off", "studio-universal", "viva"];
   final String baseUrl1 =
-      "https://firebasestorage.googleapis.com/v0/b/highlightstories-37942.appspot.com/o/";
+      "https://firebasestorage.googleapis.com/v0/b/highlightstories-37942.appspot.com/o/stories%2F";
   final String baseUrl2 =
-      ".mp4?alt=media&token=6b5c59a7-a4f4-4a0d-84c3-d849c1ad9a2d";
+      ".mp4?alt=media&token=db83b9a3-7f90-4ff4-a464-187224728b2e";
 
   List<String> getVideos() {
     List<String> finalUrl = [];
