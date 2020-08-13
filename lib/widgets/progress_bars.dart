aceacb8d1d3a720a80c0712f02611340b67571cb
Gustavo <gustavo.monteiro@dextra-sw.com>
2020-08-13 11:33:32 -0300

    Initial commit

---
+++ lib/widgets/progress_bars.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class ProgressBars extends StatefulWidget {
+
+  final List<>
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
---
+++ lib/widgets/promotional_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+
+class PromotionalScreen extends StatefulWidget {
+
+  PromotionalScreen({this.videoUrl, this.title, this.description});
+
+  final String videoUrl;
+  final String title;
+  final String description;
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
